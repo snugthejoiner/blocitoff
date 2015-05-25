5c461f1c51970426ab116f896b91563d3342d002
snugthejoiner <joiner@jedparish.com>
2015-05-24 20:33:41 -0400

    user profile added

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,6 @@
     ActionMailer::Base.smtp_settings = {
       port:                 2525,
       address:              'smtp.mailgun.org',
-      authentication:       :plain,
       user_name:            ENV['MAILGUN_SMTP_LOGIN'],
       password:             ENV['MAILGUN_SMTP_PASSWORD'],
       domain:               'heroku.com',
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  resources :users, only: [:show]
+  
+  authenticated :user do
+     root to: 'users#show', as: :authenticated_root
+  end
+
   get 'welcome/index'
 
-  devise_for :users
   root :to => "welcome#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
