25a6ca7016c57d2cb3a699fd4071c7d21ef6a55c
snugthejoiner <joiner@jedparish.com>
2015-05-28 22:00:11 -0400

    completed and uncompleted sections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+
   def show
     @user = current_user
-    @items = @user.items
+    @items = @user.items.where(completed: nil)
+    @completed_items = @user.items.where(completed: true)
   end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+
+  def completed?
+    completed == true
+  end
+  
 end
