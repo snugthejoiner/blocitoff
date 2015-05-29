2f38af504d66b55f24979fd8a2ef94134d26f526
snugthejoiner <joiner@jedparish.com>
2015-05-29 14:17:07 -0400

    final tweaks for now

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,8 @@ class ItemsController < ApplicationController
     @user = current_user
     @item = current_user.items.build(params.require(:item).permit(:name, :expires_at))
     @item.expires_at = Time.zone.now + 7.days
-    if @item.save
-      flash[:notice] = "Item was created."
-    else
-      flash[:error] = "There was an error saving the item. Please try again."
-    end
+    
+    flash[:error] = "There was an error saving the item. Please try again." unless @item.save
 
     redirect_to @user
   end
@@ -44,7 +41,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:item_id])
     @item.update_attribute(:completed, false)
     if @item.save
-      flash[:notice] = "Task Uncompleted."
+      flash[:alert] = "Task Uncompleted."
     else
       flash[:error] = "There was an error saving the item. Please try again."
     end
